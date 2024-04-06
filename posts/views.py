0a9da5c011bde4bae15d4bc257d251ff2c2e8bd7
Keval Patel <patelkeval647@gmail.com>
2024-04-06 01:14:48 -0400

    Add templates\posts\main.html and urls.py under posts directory

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list_and_create(request):
+    qs = Post.objects.all()
+    return render(request, 'posts/main.html', {'qs':qs})
--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'profiles',
     #3rd party apps
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'posts' / 'static',
