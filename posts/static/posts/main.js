8bf193030a86641fd092fa511a86b963839ce68d
Keval Patel <patelkeval647@gmail.com>
2024-04-06 20:11:49 -0400

    Create function like_unlike_post in views.py

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -5,7 +5,7 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
-function getCookie(name) {
+const getCookie =(name) => {
     let cookieValue = null;
     if (document.cookie && document.cookie !== ''){
         const cookies = document.cookie.split(';');
@@ -13,14 +13,39 @@ function getCookie(name) {
             const cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent{cookie.substring(name.length + 1)};
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
         }
     }
     return cookieValue;
 }
-const csrf_token = getCookie('csrftoken');
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = ()=> {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=>{
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response)
+            {
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    }))
+}
 
 let visible = 3
 
@@ -49,14 +74,14 @@ const getData = () =>{
                                 </div>
                                 <div class="col-2">
                                     <form class="like-unlike-forms" data-form-id="${el.id}">
-                                        {% csrf_token %}
-                                        <button href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                        <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
                                         </form>
                                 </div>
                             </div>
                         </div>
                     </div>`
             });
+            likeUnlikePosts()
         }, 100)
         console.log(response.size)
         if (response.size === 0) {
